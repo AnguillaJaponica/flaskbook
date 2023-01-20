2c724715351b6e9c8f1e3c4ae9021e3e31208be1
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2023-01-20 11:04:35 +0900

    add hello route

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Hello,  Flask!"
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}!"
