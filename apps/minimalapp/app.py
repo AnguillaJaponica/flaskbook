430537dcfc31004399af344230bbdf39bd068572
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2023-01-20 11:32:29 +0900

    use tes_context

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/hello/<name>")
 def show_name(name):
     return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    # /
+    # print(url_for("show_name"))
+    # /hello/world
+    # print(url_for("hello-endpoint", name="world"))
+    # /hello/ichiro?page=1
+    print(url_for("show_name", name="ichiro", page="1"))
