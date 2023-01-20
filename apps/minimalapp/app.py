bbb8c2f20f3a725e7758c9389af64d38d10ffafd
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2023-01-20 16:06:03 +0900

    get values from form

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, current_app, g, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,25 @@ app = Flask(__name__)
 @app.route("/hello/<name>")
 def show_name(name):
     return render_template("index.html", name=name)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        user_name = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        # メールを送る
+        # contactエンドポイントにリダイレクト
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
 
 
 with app.test_request_context():
