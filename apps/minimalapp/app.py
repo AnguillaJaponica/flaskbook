399e44bf9921c930065792d025cbe30108c71d6e
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2023-01-21 22:17:06 +0900

    add validation

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,6 +1,16 @@
-from flask import Flask, current_app, g, redirect, render_template, request, url_for
+from flask import (
+    Flask,
+    current_app,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hogefugapiyo"
 
 
 @app.route("/hello/<name>")
@@ -16,12 +26,37 @@ def contact():
 @app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
     if request.method == "POST":
-        user_name = request.form["username"]
+        username = request.form["username"]
         email = request.form["email"]
         description = request.form["description"]
 
+        # 入力チェック
+        is_valid = True
+
+        if not username:
+            flash("ユーザ名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
         # メールを送る
         # contactエンドポイントにリダイレクト
+        flash("問い合わせ内容はメールにて送信しました。ありがとうございます。")
         return redirect(url_for("contact_complete"))
 
     return render_template("contact_complete.html")
